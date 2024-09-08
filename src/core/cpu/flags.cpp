8a03bf3d6fdad8a7785e6bba426d8518be081931
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-09-08 18:48:27 +0300

    Use constants for cpu flag masks

--- src/core/cpu/flags.cpp
+++ src/core/cpu/flags.cpp
@@ -2,19 +2,27 @@
 
 namespace core::cpu
 {
-    bool flags::z() const { return (raw >> 7) & 1; }
-    bool flags::n() const { return (raw >> 6) & 1; }
-    bool flags::h() const { return (raw >> 5) & 1; }
-    bool flags::c() const { return (raw >> 4) & 1; }
+    namespace
+    {
+        constexpr uint8_t z_mask = 1 << 7;
+        constexpr uint8_t n_mask = 1 << 6;
+        constexpr uint8_t h_mask = 1 << 5;
+        constexpr uint8_t c_mask = 1 << 4;
+    } // namespace
+
+    bool flags::z() const { return raw & z_mask; }
+    bool flags::n() const { return raw & n_mask; }
+    bool flags::h() const { return raw & h_mask; }
+    bool flags::c() const { return raw & c_mask; }
 
     void flags::set(flag f, bool set)
     {
         switch (f)
         {
-        case flag::z: set ? (raw |= (1 << 7)) : (raw &= ~(1 << 7));
-        case flag::n: set ? (raw |= (1 << 6)) : (raw &= ~(1 << 6));
-        case flag::h: set ? (raw |= (1 << 5)) : (raw &= ~(1 << 5));
-        case flag::c: set ? (raw |= (1 << 4)) : (raw &= ~(1 << 4));
+        case flag::z: set ? (raw |= z_mask) : (raw &= ~z_mask);
+        case flag::n: set ? (raw |= n_mask) : (raw &= ~n_mask);
+        case flag::h: set ? (raw |= h_mask) : (raw &= ~h_mask);
+        case flag::c: set ? (raw |= c_mask) : (raw &= ~c_mask);
         }
     }
 } // namespace core::cpu
